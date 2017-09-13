b348028ab6d8c125ce44947fe1047be5991065a4
dsietz <davidsietz@yahoo.com>
2017-09-12 22:32:24 -0400

    adding notes for an executable example in src/bin/main.rs

---
+++ src/bin/main.rs
@@ -0,0 +1,14 @@
+/*
+** Test Data Generation 
+*/
+
+extern crate test_data_generation;
+//implement log4rs
+
+// This is the main function
+fn main() {
+	let dsp = DataSampleParser::new();
+    
+    // Print text to the console
+    println!("Hello World!");
+}
