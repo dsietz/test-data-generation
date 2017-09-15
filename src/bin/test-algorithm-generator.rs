07a4e5b6b04b14be81c0b3403494c8d868473c75
dsietz <davidsietz@yahoo.com>
2017-09-15 13:33:04 -0400

    working on adding utilities (executables) that use the library for a command line tool

--- src/bin/test-algorithm-generator.rs
+++ src/bin/test-algorithm-generator.rs
@@ -5,8 +5,12 @@
 extern crate test_data_generation;
 //implement log4rs
 
+use test_data_generation::test_data_generator::{data_sample_parser};
+
 // This is the main function
 fn main() {
+	use data_sample_parser::DataSampleParser;
+
 	let dsp = DataSampleParser::new();
     
     // Print text to the console
