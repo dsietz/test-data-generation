cf646ef395e500df2c643ac0d156d33f2f831643
dsietz <davidsietz@yahoo.com>
2017-10-14 11:01:48 -0400

    added test cases

--- src/test_data_generator/profile/profile.rs
+++ src/test_data_generator/profile/profile.rs
@@ -151,8 +151,7 @@ impl Profile {
 		let size = self.size_ranks.iter().find(|&&x|&x.1 >= &s).unwrap().0;	 	
 		
 		// second, determine the pattern to use
-		let pattern = self.pattern_ranks.iter().find(|x|&x.1 >= &s && x.0.len() == size as usize).unwrap().clone();	
-println!("random: {:?}, size: {:?}, pattern: {:?}",s, size, pattern.0);		
+		let pattern = self.pattern_ranks.iter().find(|x|&x.1 >= &s && x.0.len() == size as usize).unwrap().clone();		
 		
 		// build the entity using facts that adhere to the pattern 
 		
