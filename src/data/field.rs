b8ca95e6f50e1bdf45b77c7c24b91f5b05ee8561
dsietz <davidsietz@yahoo.com>
2017-11-10 16:12:33 -0500

    working on set and get data type for Field

--- src/data/field.rs
+++ src/data/field.rs
@@ -2,11 +2,18 @@
 // https://stackoverflow.com/questions/25740916/how-do-you-actually-use-dynamically-sized-types-in-rust
 // https://stackoverflow.com/questions/24857831/is-there-any-downside-to-overloading-functions-in-rust-using-a-trait-generic-f
 
-use std::any::Any;
+use std::any::{Any, TypeId};
 
 /// this trait is used to overload the set() function
 pub trait ConvertToBytes {
 	fn convert_to_bytes<'a>(self) -> Vec<u8>;
+}
+
+/// implement the trait for the integer data type
+impl ConvertToBytes for u8 {
+	fn convert_to_bytes<'a>(self) -> Vec<u8> {
+		return vec![self]
+	}
 }
 
 /// implement the trait for the String data type
@@ -29,7 +36,7 @@ pub struct Field {
 	/// the textual name of the field
 	pub name: String,
 	/// the data type of the field value
-	data_type: u8,
+	pub data_type: Option<TypeId> ,
 	/// the value of the field
 	pub	value: Vec<u8>,
 }
@@ -45,19 +52,22 @@ impl Field {
 	/// use test_data_generation::data::field::{Field, FieldType};
 	///	
 	/// fn main() {
-	/// 	let mut fld = Field::new("field_1".to_string(), FieldType::Text);
+	/// 	let mut fld = Field::new("field_1".to_string());
 	///		
 	///		fld.set("John".to_string());
 	///		println!("The value is {}",fld.get().downcast_ref::<String>().unwrap());
 	/// }
 	/// ```
-	pub fn new(name: String, field_type: FieldType) -> Field {
+	pub fn new(field_name: String) -> Field {
 		Field{
-			name: String::new(),
+			name: field_name,
+			/*
 			data_type: match field_type {
-				FieldType::Text => 1 as u8,
+				FieldType::Text => None,
 				FieldType::Number => 2 as u8
 			},
+			*/
+			data_type: None,
 			value: Vec::new(),
 		}
 	}
@@ -72,13 +82,33 @@ impl Field {
 	/// use test_data_generation::data::field::{Field, FieldType};
 	///	
 	/// fn main() {
-	/// 	let mut fld = Field::new("first name".to_string(), FieldType::Text);
+	/// 	let mut fld = Field::new("first name".to_string());
 	///     fld.set("John".to_string());
 	/// }
 	/// ```
-	pub fn set<T:ConvertToBytes>(&mut self, val:T) {
-		//println!("data type is {}",typeof(val));
+	pub fn set<T:ConvertToBytes>(&mut self, val: T) {
 		self.value = val.convert_to_bytes();
+	}
+	
+	/// This function sets the data type of the Field base on the value. 
+	/// 
+	/// #Example
+	/// 
+	/// ```
+	/// extern crate test_data_generation;
+	///
+	/// use test_data_generation::data::field::Field;
+	///	
+	/// fn main() {
+	/// 	let mut fld = Field::new("first name".to_string());
+	///     let n1 :u8 = 100;
+    ///  
+    ///		fld.set_field_type(&n1);
+	/// }
+	/// ```
+	pub fn set_field_type<T: ?Sized + Any>(&mut self, _s: &T){
+		self.data_type = Some(TypeId::of::<T>());
+		println!("data type is {:?}",self.data_type.unwrap());
 	}
 	
 	/// This function returns the value for the Field
@@ -93,7 +123,7 @@ impl Field {
 	/// use test_data_generation::data::field::{Field, FieldType};
 	///	
 	/// fn main() {
-	/// 	let mut fld = Field::new("field_1".to_string(), FieldType::Text);
+	/// 	let mut fld = Field::new("field_1".to_string());
 	///		
 	///		fld.set("John".to_string());
 	///		println!("The value is {}",fld.get().downcast_ref::<String>().unwrap());
@@ -102,4 +132,29 @@ impl Field {
 	pub fn get(&self) -> Box<Any> {
 		Box::new(String::from_utf8(self.value.to_vec()).unwrap())
 	}
+	
+	/*
+	/// This function sets the data type of the Field base on the value. 
+	/// 
+	/// #Example
+	/// 
+	/// ```
+	/// extern crate test_data_generation;
+	///
+	/// use test_data_generation::data::field::Field;
+	///	
+	/// fn main() {
+	/// 	let mut fld = Field::new("first name".to_string());
+	///     let n1 :u8 = 100;
+    ///  
+    ///		fld.set_field_type(&n1);
+	/// }
+	/// ```
+	pub fn get_field_type<T: ?Sized + Any>(&mut self, _s: &T) -> TypeId {
+		match TypeId {
+			None => Err("Field value not set"),
+			Some(self.data_type) => self.data_type.unwrap(),
+		}
+	}
+	*/
 }
--- tests/field.rs
+++ tests/field.rs
@@ -6,16 +6,34 @@ mod tests {
     
     #[test]
     // ensure a field can be set to a String value
-    fn set_field(){
-    	let mut fld =  Field::new("field_1".to_string(), FieldType::Text);
+    fn set_field_string(){
+    	let mut fld =  Field::new("field_1".to_string());
     	
     	fld.set("my value".to_string());
     }
     
     #[test]
+    // ensure a field can be set to a integer value
+    fn set_field_integer(){
+    	let mut fld =  Field::new("field_1".to_string());
+    	
+    	let n1 :u8 = 100;
+    	fld.set(n1);
+    }
+    
+    #[test]
+    // ensure the data type for the field can be set
+    fn set_field_type(){
+    	let mut fld =  Field::new("field_1".to_string());
+    	
+    	let n1 :u8 = 100;
+    	fld.set_field_type(&n1);
+    }    
+    
+    #[test]
     // ensure a field can retrieve a String value
     fn get_field(){
-    	let mut fld =  Field::new("field_1".to_string(), FieldType::Text);
+    	let mut fld =  Field::new("field_1".to_string());
     	
     	fld.set("my value".to_string());
     	assert_eq!(fld.get().downcast_ref::<String>().unwrap().len(), 8);
